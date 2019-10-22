6582c331d0d0dfb0c0a67095a3f8466e6e19e65a
小田真由香 <mayukaoda@odamayukanoMacBook-Pro.local>
2019-10-22 20:34:40 +0900

    first commit

---
+++ Assets/My Script/PleyerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PleyerController : MonoBehaviour{
+    Rigidbody rb;
+    public float speed;
+    int count;
+    public Text countText;
+    // Start is called before the first frame update
+    void Start(){
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(moveH, 0, moveV);
+        rb.AddForce(move * 10);
+    }
+    private void OnTriggerEnter (Collider other)
+    {
+        other.gameObject.SetActive(false);
+        count = count + 1;
+        SetCountText();
+    }
+    void SetCountText()
+    {
+        countText.text = "ゲット数：" + count.ToString();
+    }
+}
